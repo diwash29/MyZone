aa3b095a54d5b417d911819936a78ed9f0eb6e4e
Diwash Kapil Chettri <diwashkapil2@gmail.com>
2020-08-16 11:43:14 +0530

    Added allowed host

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y5hmuk*-4*%wor)ek2up+w+x!yx8l40j*n93#1zn!p66)k=c#2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-zoneee.herokuapp.com']
 
 
 # Application definition
